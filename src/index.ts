991d8f6f1aa8b06a1bc64120baf991b9c924c6fc
Lucas Queiroga <lucasqueirogadev@gmail.com>
2024-06-17 16:49:16 -0300

    Restart the application, to create with the new engineering

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from "./api";
-import Logger from "./config/logger";
-
-const port = process.env.PORT;
-
-app.listen(port, () => {
-  Logger.info(`You're connected on port ${port}`);
-});
