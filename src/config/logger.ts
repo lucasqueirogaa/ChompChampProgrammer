46aa0c1797295540ba02da658f81b748ca5cc0be
Lucas Queiroga <lucasqueirogadev@gmail.com>
2024-06-17 20:33:25 -0300

    feat: Configure logger

---
+++ src/config/logger.ts
@@ -0,0 +1,15 @@
+import * as log4js from "log4js";
+
+log4js.configure({
+  appenders: {
+    file: { type: "file", filename: "./src/logs/allLogs.log" },
+    console: { type: "console" },
+  },
+  categories: {
+    default: { appenders: ["console", "file"], level: "trace" },
+  },
+});
+
+const logger = log4js.getLogger("cheese");
+
+export default logger;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import logger from "./config/logger";
+
+logger.info("Testing index");
